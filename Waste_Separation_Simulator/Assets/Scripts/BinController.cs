811aa7ad496b72fc78dc0974a2d95698c5874a06
Tim-Niklas <44227439+tim-niklas@users.noreply.github.com>
2019-12-31 11:13:00 +0100

    Update bins

---
+++ Waste_Separation_Simulator/Assets/Scripts/BinController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinController : MonoBehaviour
+{
+    public string sortOfWaste = ""; //Sort of waste -> BioWaste, GlassWaste, PaperWaste, RestWaste, YellowWaste, SonderWaste
+
+    public int score = 0;
+
+    // Lights
+
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == sortOfWaste)
+        {
+            score += 10;
+            //Green Light
+        }
+        else if (collision.gameObject.tag == "BioWaste")
+        {
+            score -= 10;
+            //White Light -> BioWaste
+            //Red Light SelfWaste
+        }
+        else if (collision.gameObject.tag == "GlassWaste")
+        {
+            score -= 10;
+        }
+        else if (collision.gameObject.tag == "RestWaste")
+        {
+            score -= 10;
+        }
+        else if (collision.gameObject.tag == "YellowWaste")
+        {
+            score -= 10;
+        }
+        else if (collision.gameObject.tag == "RestWaste")
+        {
+            score -= 10;
+        }
+
+        Destroy(collision.gameObject);
+    }
+
+}

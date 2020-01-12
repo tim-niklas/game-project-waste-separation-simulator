1c78cd271d537d1bb6472411e09aa5327e195f0a
Tim-Niklas <44227439+tim-niklas@users.noreply.github.com>
2020-01-12 23:52:22 +0100

    Add difficulty

--- Waste_Separation_Simulator/Assets/Scripts/BinController.cs
+++ Waste_Separation_Simulator/Assets/Scripts/BinController.cs
@@ -14,6 +14,8 @@ public class BinController : MonoBehaviour
 
     public GameObject scoreTextObject; //get the scoreText
 
+    public GameObject diffController;
+
     //Audio
     public AudioSource audioWrongWaste;
     public AudioSource audioRightWaste;
@@ -33,7 +35,9 @@ public class BinController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     
+        //change the difficulty
+        diffController = GameObject.Find("DifficultyController");
+        minusTime = diffController.GetComponent<DifficultyController>().minusTime;
     }
 
     // Update is called once per frame
---
+++ Waste_Separation_Simulator/Assets/Scripts/DifficultyController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyController : MonoBehaviour
+{
+    public float minusTime = 5;
+    public float plusTime = 2;
+
+    public Button easy;
+    public Button medium;
+    public Button hard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void DiffEasy()
+    {
+        minusTime = 5;
+        easy.GetComponent<Image>().color = Color.grey;
+        hard.GetComponent<Image>().color = Color.white;
+        medium.GetComponent<Image>().color = Color.white;
+    } 
+    public void DiffMedium()
+    {
+        minusTime = 10;
+        medium.GetComponent<Image>().color = Color.grey;
+        easy.GetComponent<Image>().color = Color.white;
+        hard.GetComponent<Image>().color = Color.white;
+    }
+
+    public void DiffHard()
+    {
+        minusTime = 15;
+        hard.GetComponent<Image>().color = Color.grey;
+        easy.GetComponent<Image>().color = Color.white;
+        medium.GetComponent<Image>().color = Color.white;
+    }
+}

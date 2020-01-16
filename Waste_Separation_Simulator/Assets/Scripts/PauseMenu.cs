f2384e58a2566b0dd9706ac380a6b2a1b8b89b10
Starplow <starplow@live.de>
2020-01-16 17:11:47 +0100

    Pause menu not finished

---
+++ Waste_Separation_Simulator/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject germanPause;
+    public GameObject englishPause;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Menu Button") == true)
+        {
+
+        }
+    }
+}
